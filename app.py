c60c2a5976526a3bdb80c2029adfd45c549dfeaa
Aleksander Rewak <rewak.a@wp.pl>
2025-03-13 17:17:04 +0100

    Comm2

--- app.py
+++ app.py
@@ -2,22 +2,27 @@ from flask import Flask, render_template
 import mysql.connector
 import os
 
-app = Flask(__name__)
+config = {
+    'user': 'wbsadmin',  # Nazwa użytkownika bazy danych
+    'password': 'Testowe7',  # Hasło użytkownika
+    'host': 'wbspython2025.mysql.database.azure.com',  # Nazwa serwera Azure MySQL
+    'database': 'wbspython2025',  # Nazwa bazy danych
+    'port': 3306,  # Port (domyślnie 3306 dla MySQL)
+    #'ssl_ca': '/path/to/BaltimoreCyberTrustRoot.crt.pem'  # Ścieżka do certyfikatu SSL
+}
 
-def get_db_connection():
-    conn = mysql.connector.connect(
-        host=os.getenv('DATABASE_HOST'),
-        user=os.getenv('DATABASE_USER'),
-        password=os.getenv('DATABASE_PASSWORD'),
-        database=os.getenv('DATABASE_NAME')
-    )
-    return conn
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    conn = get_db_connection()
+    conn = mysql.connector.connect(**config)
+    # Tworzenie kursora
     cursor = conn.cursor()
-    cursor.execute('SELECT Id, Imię, Nazwisko, Miasto, StanCywilny FROM dane')
+    # Przykładowe zapytanie SQL
+    query = "SELECT * FROM Clients"
+    # Wykonanie zapytania
+    cursor.execute(query)
+    # Pobranie wyników
     data = cursor.fetchall()
     conn.close()
     return render_template('index.html', data=data)
